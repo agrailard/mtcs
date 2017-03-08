c8aebfc4d8158a14c1416d850134d7771570161d
grailarda <arthur.grailard@cgi.com>
2017-03-08 17:55:49 +0100

    Début Implémentation agenda

--- app/routes/calendrier.js
+++ app/routes/calendrier.js
@@ -1,11 +1,22 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Route.extend({
+	moment: Ember.inject.service(),
+  	beforeModel() {
+    	this.get('moment').setLocale('es');
+  	},
 	model() {
-		return {
-			jour_semaine: '',
-			jour: '',
-			mois: ''
+		const model = {};
+		model.date = {};
+		model.date.jour = moment().format('dddd D MMMM');
+		model.date.id = moment().format('YYYYMMDD');
+
+		model.evenements = this.store.query('evenement', {date: model.date.id});
+		model.evenements.creation = {
+			visible: false
 		};
+
+		return model;
 	}
 });
